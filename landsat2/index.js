a21f629a05faee2b33b5a0b7c01db9064fa2eeb1
Richie Carmichael <richie.carmichael@gmail.com>
2017-04-21 14:47:31 -0700

    update

---
+++ landsat2/index.js
@@ -0,0 +1,347 @@
+/* ------------------------------------------------------------
+
+   Copyright 2017 Esri
+
+   Licensed under the Apache License, Version 2.0 (the 'License');
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at:
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an 'AS IS' BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+--------------------------------------------------------------- */
+
+require([
+        'esri/Map',
+        'esri/geometry/Extent',
+        'esri/geometry/SpatialReference',
+        'esri/geometry/Point',
+        'esri/geometry/ScreenPoint',
+        'esri/views/MapView',
+        'esri/widgets/ScaleBar',
+        'dojo/string',
+        'dojo/domReady!'
+    ],
+    function (
+        Map,
+        Extent,
+        SpatialReference,
+        Point,
+        ScreenPoint,
+        MapView,
+        ScaleBar,
+        string
+    ) {
+        $(document).ready(function () {
+            // Enforce strict mode
+            'use strict';
+
+            var DEFAULT_SIZE = 300;
+            var DEFAULT_YEAR = 2017;
+            var MAX_IMAGE = 2000;
+
+            // Get extent of the Palms in Dubai
+            var palms1 = $('.rc-bookmark li a').get(7);
+            var palms2 = $(palms1).attr('data-extent').split(',');
+
+            // Map view
+            var _view = new MapView({
+                container: 'map',
+                extent: new Extent({
+                    xmin: Number(palms2[0]),
+                    ymin: Number(palms2[1]),
+                    xmax: Number(palms2[2]),
+                    ymax: Number(palms2[3]),
+                    spatialReference: SpatialReference.WebMercator
+                }),
+                padding: {
+                    left: 0,
+                    top: 0,
+                    right: 0,
+                    bottom: 0
+                },
+                ui: {
+                    components: [
+                        'zoom'
+                    ]
+                },
+                map: new Map({
+                    basemap: 'osm'
+                })
+            });
+            _view.then(function () {
+                addLens(
+                    DEFAULT_YEAR,
+                    DEFAULT_SIZE
+                );
+            });
+
+            //var navigating = false;
+            _view.watch('extent', $.debounce(250, true, function (e) {
+                clearLensImages();
+            }));
+            _view.watch('extent', $.debounce(250, function (e) {
+                updateLensImages();
+            }));
+
+            // Add a scalebar to the lower left hand corner.
+            _view.ui.add(new ScaleBar({ view: _view }), 'bottom-left');
+
+            // Respond to menu item selection.
+            $('.dropdown-menu li a').click(function () {
+                if ($(this).parent().parent().hasClass('rc-bookmark')) {
+                    // Zoom to user defined spatial bookmark.
+                    var coordinates = $(this).attr('data-extent').split(',');
+                    _view.extent = new Extent({
+                        xmin: Number(coordinates[0]),
+                        ymin: Number(coordinates[1]),
+                        xmax: Number(coordinates[2]),
+                        ymax: Number(coordinates[3]),
+                        spatialReference: SpatialReference.WebMercator
+                    });
+
+                    // Update map lens image.
+                    clearLensImages();
+                    updateLensImages();
+                }
+
+                if ($(this).parent().parent().hasClass('rc-theme')) {
+                    // Exit if item already selected.
+                    if ($(this).parent().hasClass('active')) { return; }
+
+                    // Toggle enabled state for clicked item and siblings.
+                    $(this).parent().addClass('active').siblings().removeClass('active');
+
+                    //
+                    var theme = $('.rc-theme li.active a').html();
+                    $('.rc-theme').siblings('a').find('.rc-item-value').html(theme);
+                    
+                    // Update map lens image.
+                    clearLensImages();
+                    updateLensImages();
+                }
+
+                if ($(this).parent().parent().hasClass('rc-year')) {
+                    // Get year.
+                    var year = $(this).attr('data-year');
+                    
+                    //
+                    if (year === 'close-all'){
+                        $('.rc-window').remove();
+                        return;
+                    }
+
+                    // Add new lens.
+                    addLens(
+                        year,
+                        DEFAULT_SIZE
+                    );
+                }
+            });
+
+            // Display help and about dialogs.
+            $('#buttonHelp').click(function () {
+                $('#modalHelp').modal('show');
+            });
+            $('#buttonAbout').click(function () {
+                $('#modalAbout').modal('show');
+            });
+
+            // Function that adds a new lens.
+            function addLens(year, size) {
+                // 
+                var left = _view.width / 2 - size / 2;
+                var top = _view.height / 2 - size / 2;
+
+                var window = $(document.createElement('div')).addClass('rc-window').css({
+                    position: 'absolute',
+                    left: left + 'px',
+                    top: top + 'px',
+                    width: size + 'px',
+                    height: size + 'px'
+                }).data({
+                    year: year
+                });
+
+                window.touch({
+                    canTranslate: true,
+                    canRotate: true,
+                    canScale: true,
+                    touchStart: function (e) {
+                        // Bring lens to the front
+                        $(e.object).bringToFont('.rc-window');
+
+                        // 
+                        $(e.object).css({
+                            //opacity: 0.7,
+                            cursor: 'grabbing'
+                        });
+                    },
+                    touchMove: function (e) {
+                        // Create texture transformation
+                        var transform = string.substitute('translate(${x}px,${y}px) scale(${s}) rotate(${r}deg)', {
+                            x: -e.x,
+                            y: -e.y,
+                            s: 1 / e.s,
+                            r: -e.r
+                        });
+
+                        // Create texture transformation origin
+                        var origin = string.substitute('${x}px ${y}px', {
+                            x: _view.width / 2 + e.x,
+                            y: _view.height / 2 + e.y
+                        });
+
+                        // Apply transformation and transformation origin
+                        $(e.object).children('.rc-window-image').css({
+                            '-webkit-transform': transform,
+                            '-moz-transform': transform,
+                            '-ms-transform': transform,
+                            '-0-transform': transform,
+                            'transform': transform,
+                            '-webkit-transform-origin': origin,
+                            '-moz-transform-origin': origin,
+                            '-ms-transform-origin': origin,
+                            '-0-transform-origin': origin,
+                            'transform-origin': origin
+                        });
+                    },
+                    touchEnd: function (e) {
+                        //
+                        $(e.object).css({
+                            //opacity: 1,
+                            cursor: 'grab'
+                        });
+                    }
+                });
+
+                // Add animated progress gif
+                window.append(
+                    $(document.createElement('img'))
+                        .attr('src', 'img/loading-throb.gif')
+                        .css({
+                            position: 'absolute',
+                            left:  '50%',
+                            top: '50%',
+                            width: '33px',
+                            'margin-top': '-20px',
+                            'margin-left': '-17px',
+                            'pointer-events': 'none'
+                        })
+                );
+
+                // Add background map texture
+                window.append(
+                    $(document.createElement('div')).addClass('rc-window-image').css({
+                        position: 'absolute',
+                        left: -left + 'px',
+                        top: -top + 'px',
+                        width: _view.width + 'px',
+                        height: _view.height + 'px',
+                        'pointer-events': 'none',
+                        'background-size': 'cover',
+                        'background-repeat': 'no-repeat',
+                        'background-image': 'none'
+                    })
+                );
+
+                // Add year text
+                window.append(
+                    $(document.createElement('div')).css({
+                        position: 'absolute',
+                        left: '0',
+                        top: '0',
+                        color: 'rgba(255, 255, 255, 0.5)',
+                        'font-size': '24px',
+                        'font-weight': 700,
+                        'pointer-events': 'none',
+                        'margin-top': '1px',
+                        'margin-left': '5px'
+                    }).html(year)
+                );
+
+                // Add window to window container
+                $('#map-container').append(
+                    window
+                );
+
+                // Update lens texture
+                clearLensImages();
+                updateLensImages();
+            }
+
+            function getImageUrl(year) {
+                // Calcuate scale (if image width or height exceeds maximum).
+                var max = Math.max(_view.width, _view.height);
+                var scale = max <= MAX_IMAGE ? 1 : MAX_IMAGE / max;
+
+                // Get url/function from user defined selection.
+                var url = $('.rc-theme li.active a').attr('data-url');
+                var fxn = $('.rc-theme li.active a').attr('data-function');
+
+                // Construct map request url.
+                var date =
+                    url += '/exportImage?f=image'
+                url += string.substitute('&bbox=${xmin},${ymin},${xmax},${ymax}', {
+                    xmin: _view.extent.xmin,
+                    ymin: _view.extent.ymin,
+                    xmax: _view.extent.xmax,
+                    ymax: _view.extent.ymax
+                });
+                url += '&bboxSR=' + _view.spatialReference.wkid;
+                url += '&imageSR=' + _view.spatialReference.wkid;
+                url += string.substitute('&size=${w},${h}', {
+                    w: Math.min(Math.round(scale * _view.width), MAX_IMAGE),
+                    h: Math.min(Math.round(scale * _view.height), MAX_IMAGE)
+                });
+                url += string.substitute('&time=${f},${t}', {
+                    f: 0,
+                    t: Date.UTC(year, 0, 1)
+                });
+                url += '&format=' + 'jpgpng';
+                url += '&interpolation=' + 'RSP_BilinearInterpolation';
+                url += '&mosaicRule=' + '{mosaicMethod:\'esriMosaicAttribute\',sortField:\'AcquisitionDate\',sortValue:\'2017/02/06, 12:00 AM\',ascending:true,mosaicOperation:\'MT_FIRST\',where:\'CloudCover<=0.1\'}';
+                url += '&renderingRule=' + string.substitute('{rasterFunction:\'${rasterFunction}\'}', {
+                    rasterFunction: fxn
+                });
+                return url;
+            }
+
+            function clearLensImages() {
+                $('.rc-window').each(function () {
+                    $(this).children('.rc-window-image').css({
+                        'background-image': 'none'
+                    });
+                });
+            }
+            function updateLensImages() {
+                $('.rc-window').each(function () {
+                    var year = $(this).data().year;
+                    $(this).children('.rc-window-image').css({
+                        width: _view.width + 'px',
+                        height: _view.height + 'px',
+                        'background-image': function () {
+                            return 'url("' + getImageUrl(year) + '")';
+                        }
+                    });
+                });
+            }
+        });
+
+        $.fn.bringToFont = function (selector) {
+            var max = Math.max.apply(null, $(this).siblings(selector).map(function () {
+                return Number($(this).css('z-index'));
+            }));
+            if (max >= Number($(this).css('z-index'))) {
+                $(this).css({
+                    'z-index': ++max
+                });
+            }
+            return this;
+        };
+    }
+);
---
+++ landsat2/js/jquery.touch.js
@@ -0,0 +1,386 @@
+/* ------------------------------------------------------------
+
+   Copyright 2017 Esri
+
+   Licensed under the Apache License, Version 2.0 (the 'License');
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at:
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an 'AS IS' BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+--------------------------------------------------------------- */
+
+(function ($) {
+    // Enforce strict mode
+    'use strict';
+
+    var defaults = {
+        canTranslate: true,
+        canScale: true,
+        canRotate: true,
+        touchStart: null,
+        touchMove: null,
+        touchEnd: null
+    };
+
+    //
+    var props = $.event.props || [];
+    props.push('touches');
+    props.push('scale');
+    props.push('rotation');
+    $.event.props = props;
+
+    function Touch(element, options) {
+        // Store element
+        this.element = element;
+
+        // Initialize transformation parameters.
+        this.x = 0;
+        this.y = 0;
+        this.r = 0;
+        this.s = 1;
+
+        // Initialize transformation deltas.
+        this.dx = 0;
+        this.dy = 0;
+        this.dr = 0;
+        this.ds = 1;
+
+        //
+        this.options = $.extend({}, defaults, options);
+
+        //
+        this.origin = {};
+
+        //
+        $(this.element).on('touchstart.touch', this.touchstart.bind(this));
+        $(this.element).on('touchmove.touch', this.touchmove.bind(this));
+        $(this.element).on('touchend.touch touchcancel.touch', this.touchend.bind(this));
+        $(this.element).on('mousedown.touch', this.mousestart.bind(this));
+        $(this.element).on('mousemove.touch', this.mousemove.bind(this));
+        $(this.element).on('mouseup.touch', this.mouseend.bind(this));
+        $(this.element).on('mousewheel.touch', this.mousewheel.bind(this));
+    }
+
+    Touch.prototype.mousewheel = function (e) {
+        // If scale enabled then enlarge or reduce.
+        if (this.options.canScale) {
+            // Prepare transformation description.
+            this.s *= (1 + e.deltaY * 0.01);
+
+            // Apply transformation.
+            this.applyTransform(
+                this.x,
+                this.y,
+                this.r,
+                this.s
+            );
+        }
+    }
+
+    Touch.prototype.mousestart = function (e) {
+        // Store mouse start position
+        this.origin['mouse'] = {
+            x: e.pageX,
+            y: e.pageY
+        }
+
+        // Invoke touch-start callback
+        if (this.options.touchStart) {
+            this.options.touchStart({
+                object: this.element
+            });
+        }
+    }
+
+    Touch.prototype.mousemove = function (e) {
+        // Exit if no mouse origin.
+        if (!this.origin['mouse']) { return; }
+
+        // Exit if translation not supported.
+        this.dx = 0;
+        this.dy = 0;
+        if (this.options.canTranslate) {
+            //
+            this.dx = e.pageX - this.origin['mouse'].x;
+            this.dy = e.pageY - this.origin['mouse'].y;
+        }
+
+        //
+        this.applyTransform(
+            this.x + this.dx,
+            this.y + this.dy,
+            this.r,
+            this.s
+        );
+    }
+
+    Touch.prototype.mouseend = function (e) {
+        // Clear origin
+        this.origin = {};
+
+        // Exit if translation not supported.
+        if (this.options.canTranslate) {
+            //
+            this.x += this.dx;
+            this.y += this.dy;
+        }
+
+        // Invoke touch-end callback.
+        if (this.options.touchEnd) {
+            this.options.touchEnd({
+                object: this.element,
+                x: this.x,
+                y: this.y,
+                r: this.r,
+                s: this.s
+            });
+        }
+    }
+
+    Touch.prototype.touchstart = function (e) {
+        // Prevent event bubbling.
+        e.preventDefault();
+        
+        // Invoke touch-start callback
+        if (this.options.touchStart) {
+            this.options.touchStart({
+                object: this.element
+            });
+        }
+
+        // Store originating touch positions.
+        for (var i = 0; i < e.touches.length; i++) {
+            var touch = e.touches.item(i);
+            if (touch.target === this.element) {
+                if (touch.identifier in this.origin) {
+                    // Finger already tracked
+                }
+                else {
+                    this.origin[touch.identifier] = {
+                        x: touch.pageX,
+                        y: touch.pageY
+                    }
+                }
+            }
+        }
+    };
+
+    Touch.prototype.touchmove = function (e) {
+        // Prevent event bubbling.
+        e.preventDefault();
+
+        // Filter events not related to current element.
+        var touches = [];
+        for (var i = 0; i < e.touches.length; i++) {
+            var touch = e.touches.item(i);
+            if (touch.target === this.element) {
+                touches[touch.identifier] = {
+                    x: touch.pageX,
+                    y: touch.pageY
+                }
+            }
+        }
+
+        // Get Fingers
+        var fingers1 = Object.keys(this.origin);
+        var fingers2 = Object.keys(touches);
+
+        // Exit if no fingers touching.
+        if (fingers1.length === 0 || fingers2.length === 0) { return; }
+        if (fingers1.length !== fingers2.length) { return; }
+        var count = fingers1.length;
+
+        // Calculate translation deltas.
+        this.dx = 0;
+        this.dy = 0;
+        if (this.options.canTranslate) {
+            for (var i = 0; i < count; i++) {
+                var id = fingers1[i];
+                this.dx += touches[id].x - this.origin[id].x
+                this.dy += touches[id].y - this.origin[id].y
+            }
+            this.dx /= fingers1.length;
+            this.dy /= fingers1.length;
+        }
+
+        // Calculate rotation deltas.
+        this.dr = 0;
+        if (this.options.canRotate) {
+            if (count > 1) {
+                // Find center of original hand 
+                var hand1x = 0;
+                var hand1y = 0;
+                for (var i = 0; i < count; i++) {
+                    var id = fingers1[i];
+                    hand1x += this.origin[id].x;
+                    hand1y += this.origin[id].y;
+                }
+                hand1x /= count;
+                hand1y /= count;
+
+                // Find center of current hand 
+                var hand2x = 0;
+                var hand2y = 0;
+                for (var i = 0; i < count; i++) {
+                    var id = fingers2[i];
+                    hand2x += touches[id].x;
+                    hand2y += touches[id].y;
+                }
+                hand2x /= count;
+                hand2y /= count;
+
+                // Set angle to center
+                for (var i = 0; i < count; i++) {
+                    var id = fingers1[i];
+                    var r1 = Math.atan2(
+                        this.origin[id].y - hand1y,
+                        this.origin[id].x - hand1x
+                    );
+                    var r2 = Math.atan2(
+                        touches[id].y - hand2y,
+                        touches[id].x - hand2x
+                    );
+                    this.dr += r2 - r1;
+                }
+            }
+            //
+            this.dr *= 180 / Math.PI;
+            this.dr /= count;
+        }
+
+        // Calculate scale deltas.
+        this.ds = 1;
+        if (this.options.canScale) {
+            if (touches.length > 1) {
+                this.ds = 0;
+
+                // Find size of original hand
+                var minx1 = null;
+                var miny1 = null;
+                var maxx1 = null;
+                var maxy1 = null;
+                for (var i = 0; i < count; i++) {
+                    var id = fingers1[i];
+                    var x = this.origin[id].x;
+                    var y = this.origin[id].y;
+                    minx1 = minx1 === null ? x : Math.min(minx1, x);
+                    miny1 = miny1 === null ? y : Math.min(miny1, y);
+                    maxx1 = maxx1 === null ? x : Math.max(maxx1, x);
+                    maxy1 = maxy1 === null ? y : Math.max(maxy1, y);
+                }
+
+                // Find size of original hand
+                var minx2 = null;
+                var miny2 = null;
+                var maxx2 = null;
+                var maxy2 = null;
+                for (var i = 0; i < count; i++) {
+                    var id = fingers2[i];
+                    var x = touches[id].x;
+                    var y = touches[id].y;
+                    minx2 = minx2 === null ? x : Math.min(minx2, x);
+                    miny2 = miny2 === null ? y : Math.min(miny2, y);
+                    maxx2 = maxx2 === null ? x : Math.max(maxx2, x);
+                    maxy2 = maxy2 === null ? y : Math.max(maxy2, y);
+                }
+
+                // 
+                var scalex = (maxx2 - minx2) / (maxx1 - minx1);
+                var scaley = (maxy2 - miny2) / (maxy1 - miny1);
+                this.ds = Math.max(scalex, scaley) / 2;
+            }
+        }
+
+        //
+        this.applyTransform(
+            this.x + this.dx,
+            this.y + this.dy,
+            (this.r + this.dr) % 360,
+            this.s * this.ds
+        );
+    };
+
+    Touch.prototype.touchend = function (e) {
+        // Prevent event bubbling.
+        e.preventDefault();
+
+        // Is this the last finger? If so, do not proceed.
+        var fingerCount = 0;
+        for (var i = 0; i < e.touches.length; i++) {
+            var touch = e.touches.item(i);
+            if (touch.target === this.element) {
+                fingerCount++;
+            }
+        }
+        if (fingerCount !== 0) { return; }
+
+        // Clear origin
+        this.origin = {};
+
+        // Store last used transformation parameters.
+        this.x += this.dx;
+        this.y += this.dy;
+        this.r = (this.r + this.dr) % 360;
+        this.s *= this.ds;
+
+        // Invoke touch-end callback.
+        if (this.options.touchEnd) {
+            this.options.touchEnd({
+                object: this.element
+            });
+        }
+    };
+
+    Touch.prototype.applyTransform = function (x, y, r, s) {
+        // Build CSS tranform property.
+        var transform =
+            'translate(' + x + 'px,' + y + 'px) ' +
+            'scale(' + s + ') ' +
+            'rotate(' + r + 'deg)';
+
+        // Apply transformation.
+        $(this.element).css({
+            '-webkit-transform': transform,
+            '-moz-transform': transform,
+            '-ms-transform': transform,
+            '-0-transform': transform,
+            'transform': transform
+        });
+
+        // Invoke touch-move callback.
+        if (this.options.touchMove) {
+            this.options.touchMove({
+                object: this.element,
+                x: x,
+                y: y,
+                r: r,
+                s: s
+            });
+        }
+    }
+
+    // jQuery function to enable touch.
+    $.fn.touch = function (options) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_touch')) {
+                $.data(this, 'plugin_touch', new Touch(this, options));
+            }
+        });
+    };
+
+    // jQuery function to disable touch.
+    $.fn.untouch = function () {
+        return this.each(function () {
+            if ($.data(this, 'plugin_touch')) {
+                $.removeData(this, 'plugin_touch');
+            }
+            $(this).off('.touch');
+        });
+    };
+}(jQuery));
